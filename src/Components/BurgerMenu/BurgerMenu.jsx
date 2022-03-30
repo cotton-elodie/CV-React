2b97270cf514737930c90bcf80e99f5044fe7592
Elodie Cotton <elodie.cotton@hotmail.com>
2022-03-30 09:10:45 +0200

    first commit

--- src/Components/BurgerMenu/BurgerMenu.jsx
+++ src/Components/BurgerMenu/BurgerMenu.jsx
@@ -4,30 +4,31 @@ import { Link } from "react-router-dom";
 import "./burgerMenu.css";
 
 const toggleMenu = ({ isOpen }) => {
-    const menuWrap = document.querySelector(".bm-menu-wrap");
-    isOpen
-      ? menuWrap.setAttribute("aria-hidden", false)
-      : menuWrap.setAttribute("aria-hidden", true);
-  };
+  const menuWrap = document.querySelector(".bm-menu-wrap");
+  isOpen
+    ? menuWrap.setAttribute("aria-hidden", false)
+    : menuWrap.setAttribute("aria-hidden", true);
+};
 
 const BurgerMenu = () => {
-    
-
   return (
     <Menu noOverlay onStateChange={toggleMenu}>
-        <Link className="menu-item" to="/">A propos de moi</Link>
+      {/* <Link className="menu-item" to="/">A propos de moi</Link>
         <Link className="menu-item" to="/experiences">Experiences</Link>
         <Link className="menu-item" to="/formations">Formations</Link>
-        <Link className="menu-item" to="/competences">Compétences</Link>
-      {/* <a className="menu-item" href="/">
-        Home
+        <Link className="menu-item" to="/competences">Compétences</Link> */}
+      <a className="menu-item" href="/">
+        A propos de moi
       </a>
-      <a className="menu-item" href="/about">
-        About
+      <a className="menu-item" href="/experiences">
+        Experiences
       </a>
-      <a className="menu-item" href="/contact">
-        Contact
-      </a> */}
+      <a className="menu-item" href="/formations">
+        Formations
+      </a>
+      <a className="menu-item" href="/competences">
+        Compétences
+      </a>
     </Menu>
   );
 };
